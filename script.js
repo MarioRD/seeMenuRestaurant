b1dadce15866a2171974ac2025884a4687aa8e69
MarioRD <74079359+MarioRD@users.noreply.github.com>
2024-06-20 20:55:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ document.getElementById('openCameraButton').addEventListener('click', async () =
     const video = document.getElementById('video');
 
     try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const constraints = {
+            video: {
+                facingMode: { exact: 'environment' }
+            }
+        };
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
         video.srcObject = stream;
         video.style.display = 'block';
     } catch (error) {
         console.error('Error accessing the camera:', error);
-        alert('No se pudo acceder a la cámara. Por favor, verifica los permisos y vuelve a intentarlo.');
+        alert('No se pudo acceder a la cámara. Por favor, verifica los permisos en la configuración de tu navegador y vuelve a intentarlo.');
     }
 });
+
