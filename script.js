8d2a14efdf6ca16244c59971a2a6990688fd8424
MarioRD <74079359+MarioRD@users.noreply.github.com>
2024-06-21 21:03:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -39,8 +39,10 @@ let animationFrame;
 
 function startAnimation() {
     const model = document.getElementById('animatedModel');
+    model.object3D.scale.set(0.05, 0.05, 0.05); // Ajustar escala dinámicamente
+    model.object3D.rotation.set(Math.PI / 2, 0, 0); // Ajustar rotación dinámicamente
     const animate = () => {
-        model.object3D.rotation.y += 0.005; // Solo rotar en el eje X
+        model.object3D.rotation.x += 0.01; // Solo rotar en el eje X
         animationFrame = requestAnimationFrame(animate);
     };
     animate();
